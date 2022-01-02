9717736bb60ef7d35920ab1cea625ff2716680ae
Yudai NAKATA <yudai.nkt@gmail.com>
2022-01-03 03:19:22 +0900

    chore: explain why the dummy TS file is required
    The issue typescript-eslint/typescript-eslint#4360 explains the reason.

--- __tests__/dummy.ts
+++ __tests__/dummy.ts
@@ -1,2 +1,3 @@
 // This empty file is present to ensure that __tests__/dummy.ts is
 // actually in the filesystem during the test.
+// cf. https://github.com/typescript-eslint/typescript-eslint/issues/4360
